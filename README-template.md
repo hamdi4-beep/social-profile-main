fd52d2a24a67477e38af6cab50c92c3d4f4a742a
Hamdi Kamel <hamdiakippuden@gmail.com>
2024-03-11 07:56:20 -0700

    refactored code to make it cleaner and more structured

---
+++ README-template.md
@@ -0,0 +1,11 @@
+# Social Profile Links
+
+## Overview
+A frontend mentor challenge I completed to practice React.
+
+## Takeaways
+Here's the things I've taken away from working on this project:
+
+Learned how to share a localhost server on a network.
+Practiced working with Git commands, such as 'checkout', 'reset', 'restore'.
+Got to 
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
-import avatarUrl from './assets/images/avatar-jessica.jpeg'
+import avatarUrl from './assets/images/avatar-hamdi.jpg'
 import './sass/index.scss'
+
+function ProfileLinksContainer() {
+  return (
+    <div className="profile-links">
+      <button>
+        <a href="http://github.com/hamdi4-beep">GitHub</a>
+      </button>
+
+      <button>
+        <a href="https://www.frontendmentor.io/profile/hamdi4-beep">Frontend Mentor</a>
+      </button>
+      
+      <button>
+        <a href="https://www.linkedin.com/in/hamdi-kamel-bab017b8/">LinkedIn</a>
+      </button>
+      
+      <button>
+        <a href="https://twitter.com/Hamdi33727935">Twitter</a>
+      </button>
+      
+      
+      <button>
+        <a href="https://www.instagram.com/hamdi_beep4/?igsh=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr">Instagram</a>
+      </button>
+    </div>
+  )
+}
 
 function ProfileContainer() {
   return (
@@ -9,29 +36,13 @@ function ProfileContainer() {
           <img src={avatarUrl} alt="jessica's profile picture" />
         </div>
 
-        <h3 className='name'>Jessica Randall</h3>
-        <p className="location">London, United Kingdom</p>
+        <h3 className='name'>Hamdi Kamel</h3>
+        <p className="location">Bizert, Tunisia</p>
         <p className="bio">"Front-end developer and avid reader."</p>
-      </div>
-      
-      <div className="profile-links">
-        <Button title='GitHub' />
-        <Button title='Frontend Mentor' />
-        <Button title='LinkedIn' />
-        <Button title='Twitter' />
-        <Button title='Instagram' />
+
+        <ProfileLinksContainer />
       </div>
     </div>
-  )
-}
-
-function Button({
-  title
-}: {
-  title: string
-}) {
-  return (
-    <button>{title}</button>
   )
 }
 
